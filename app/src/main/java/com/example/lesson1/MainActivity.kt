f6fb2bc1403d4b53d78bfa2ef37e2079a48b3388
mistertigerson <kg.ukr1996@gmail.com>
2021-11-07 16:29:14 +0600

    thank you for lesson

--- app/src/main/java/com/example/lesson1/MainActivity.kt
+++ app/src/main/java/com/example/lesson1/MainActivity.kt
@@ -1,38 +1,60 @@
 package com.example.lesson1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.lesson1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    companion object{
-        val EXTRA_MESSAGE = "hello world"
-    }
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        binding.editText.setText(intent.getStringExtra(MainActivity2.MESSAGE))
+        setContentView(binding.root)
+        registerForActivity()
         onClickButton()
+
 
     }
 
-    fun onClickButton(){
-        val intent = Intent(this, MainActivity2::class.java).apply {}
+
+
+    private fun onClickButton(){
+        binding.editText.setText(intent.getStringExtra(EXTRA_MESSAGE))
         binding.clickBtn.setOnClickListener() {
-            if (binding.editText.text.isEmpty()){
-                Toast.makeText(this, "EditText не может быть пустым", Toast.LENGTH_SHORT).show()
+            if (binding.editText.text.toString().isEmpty()){
+                Toast.makeText(this, getString(R.string.toastEmpty), Toast.LENGTH_SHORT).show()
             }else {
-                intent.putExtra(
-                    EXTRA_MESSAGE, binding.editText.text.toString())
-                startActivity(intent)
+                openActivityForResult()
             }
         }
     }
+
+    private fun registerForActivity() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            binding.editText.setText(result.data?.getStringExtra(TEXT))
+        }}
+    }
+
+    private fun openActivityForResult(){
+        Intent(this, MainActivity2::class.java).apply {
+            putExtra(TEXT, binding.editText.text.toString())
+            resultLauncher.launch(this)
+        }
+    }
+
+    companion object{
+        const val TEXT = "text"
+    }
+
 }
--- app/src/main/java/com/example/lesson1/MainActivity2.kt
+++ app/src/main/java/com/example/lesson1/MainActivity2.kt
@@ -1,39 +1,53 @@
 package com.example.lesson1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.lesson1.MainActivity.Companion.TEXT
 import com.example.lesson1.databinding.ActivityMain2Binding
 
 
 class MainActivity2 : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMain2Binding
-    companion object{
-        val MESSAGE = "hello world"
-    }
+    private lateinit var viewBinding : ActivityMain2Binding
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
-        binding.editText.setText(message)
+        viewBinding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        registerForActivity()
         onClick()
     }
 
     fun onClick(){
-        val intent = Intent(this, MainActivity::class.java).apply {}
-        binding.clickBtn.setOnClickListener(){
-            if (binding.editText.text.isEmpty()){
+        viewBinding.editText.setText(intent.getStringExtra(TEXT))
+        viewBinding.clickBtn.setOnClickListener(){
+            if (viewBinding.editText.text.isEmpty()){
                 Toast.makeText(this, "EditText не может быть пустым", Toast.LENGTH_SHORT).show()
-            }else{
-                intent.putExtra(MESSAGE, binding.editText.text.toString())
-                startActivity(intent)
-            }
+            }else openActivityForResult()
         }
+    }
+
+    private fun registerForActivity() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK){
+                    viewBinding.editText.setText(result.data?.getStringExtra(TEXT))
+                }}
+    }
+    fun openActivityForResult(){
+        Intent(this, MainActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, viewBinding.editText.text.toString())
+            setResult(Activity.RESULT_OK, this)
+            finish()
+        }
+
     }
 }
